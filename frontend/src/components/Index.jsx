aedf51f519f37143e9d13dccb8b92697e6c5a31d
SSiidduu-2005 <132639773+SSiidduu-2005@users.noreply.github.com>
2025-06-26 13:58:07 +0530

    jfjff

--- frontend/src/components/Index.jsx
+++ frontend/src/components/Index.jsx
@@ -6,19 +6,19 @@ function Index(){
             <div className='pi'>Popular Cricketers</div> <br />
 
             <div className='img'>
-                <div ><img src="msd.jpg" alt="MSD" width={"350em"} height={"400em"}/>
+                <div ><img src="msd.jpg" alt="MSD" width={"390em"} height={"400em"}/>
                     <br />
                     <p>Ms Dhoni (2004-2019)</p>
                 </div>
-                <div ><img src="virat.webp" alt="VK" width={"350em"} height={"400em"} />
+                <div ><img src="virat.webp" alt="VK" width={"390em"} height={"400em"} />
                 <br />
                 <p>V Kohli (2010)</p>
                 </div>
-                <div><img src="rohit.jpg" alt="RS"  width={"350em"} height={"400em"}/>
+                <div><img src="rohit.jpg" alt="RS"  width={"390em"} height={"400em"}/>
                 <br />
                 <p>R Sharma (2007)</p>
                 </div>
-                <div><img src="HP.jpg" alt="HP"  width={"350em"} height={"400em"}/>
+                <div><img src="HP.jpg" alt="HP"  width={"380em"} height={"400em"}/>
                 <br />
                 <p>H pandeya (2015)</p>
                 </div>
@@ -27,39 +27,81 @@ function Index(){
             
                 <div className='pi'>Popular places</div><br /><br />
                 <div className='img2'>
-                <div ><img src="hyd.jpg" alt="Not load"  width={"350em"} height={"300em"}/>
+                <div ><img src="hyd.jpg" alt="Not load"  width={"390em"} height={"300em"}/>
                 <br />
                 <p>Hyderabad</p>
                 </div>
-                <div ><img src="del.jpg" alt="Not load"  width={"350em"} height={"300em"}/>
+                <div ><img src="del.jpg" alt="Not load"  width={"390em"} height={"300em"}/>
                                 <br />
                 <p>Delhi</p>
                 </div>
-                <div ><img src="mi.jpg" alt="Not load"  width={"350em"} height={"300em"}/>
+                <div ><img src="mi.jpg" alt="Not load"  width={"390em"} height={"300em"}/>
                                 <br />
                 <p>Mumbai</p>
                 </div>
-                 <div ><img src="b.webp" alt="Not load"  width={"350em"} height={"300em"}/>                <br />
+                 <div ><img src="b.webp" alt="Not load"  width={"370em"} height={"300em"}/>                <br />
                 <p>Banglore</p></div>
             </div>
             <br /><br />
-
-                <div className='pi'>Popular <Cars></Cars></div><br /><br />
-                <div className='img3'>
-                <div ><img src="audi.jpg" alt="Not load"  width={"350em"} height={"300em"}/>
+            <div className='pi'>Popular Cars</div><br /><br />
+            <div className='img4'>
+                <div ><img src="audi.jpg" alt="Not load"  width={"390em"} height={"300em"}/>
                 <br />
                 <p>Audi</p>
                 </div>
-                <div ><img src="abz.jpg" alt="Not load"  width={"350em"} height={"300em"}/>
-                                <br />
-                <p>abz</p>
+                    <div ><img src="abz.jpg" alt="Not load"  width={"390em"} height={"300em"}/>
+                     <br />
+                <p>ABZ</p>
                 </div>
-                <div ><img src="benz.jpg" alt="Not load"  width={"350em"} height={"300em"}/>
+                                <div ><img src="benz.avif" alt="Not load"  width={"390em"} height={"300em"}/>
                                 <br />
-                <p>benz</p>
+                <p>Benz</p>
                 </div>
-                 <div ><img src="car.jpg" alt="Not load"  width={"350em"} height={"300em"}/>                <br />
-                <p>monterey</p></div>
+                                <div ><img src="car.jpg" alt="Not load"  width={"370em"} height={"300em"}/>
+                                <br />
+                <p>Moneterary</p>
+                </div>
+            </div>
+                        <br /><br />
+            <div className='pi'>Popular Bikes</div><br /><br />
+            <div className='img4'>
+                <div ><img src="ninja.avif" alt="Not load"  width={"390em"} height={"300em"}/>
+                <br />
+                <p>ninja</p>
+                </div>
+                    <div ><img src="bullet.avif" alt="Not load"  width={"390em"} height={"300em"}/>
+                     <br />
+                <p>bullet</p>
+                </div>
+                                <div ><img src="ns.jpeg" alt="Not load"  width={"390em"} height={"300em"}/>
+                                <br />
+                <p>NS</p>
+                </div>
+                                <div ><img src="duke.webp" alt="Not load"  width={"450em"} height={"300em"}/>
+                                <br />
+                <p>Duke</p>
+                </div>
+            </div>
+
+                                    <br /><br />
+            <div className='pi'>Popular Actors</div><br /><br />
+            <div className='img5'>
+                <div ><img src="pb.jpg" alt="Not load"  width={"390em"} height={"400em"}/>
+                <br />
+                <p>Prabhas</p>
+                </div>
+                    <div ><img src="rc.jpg" alt="Not load"  width={"390em"} height={"400em"}/>
+                     <br />
+                <p>Ram Charan</p>
+                </div>
+                                <div ><img src="hr.jpg" alt="Not load"  width={"390em"} height={"400em"}/>
+                                <br />
+                <p>Hritik Roshan</p>
+                </div>
+                                <div ><img src="pspk.jpg" alt="Not load"  width={"390em"} height={"400em"}/>
+                                <br />
+                <p>Pawan Kalyan</p>
+                </div>
             </div>
 
         </div>
